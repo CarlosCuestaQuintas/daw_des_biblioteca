e4d685e7038d242a3a58861e5a5de533c2a18d81
CarlosCuestaQuintas <carlos.cuesta.quintas@alumnojoyfe.iepgroup.es>
2025-06-10 20:44:17 +0200

    Añadido cors en los controladores

--- src/main/java/com/biblioteca/controllers/LibroController.java
+++ src/main/java/com/biblioteca/controllers/LibroController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api/libros")
 public class LibroController {
 	
--- src/main/java/com/biblioteca/controllers/UsuarioController.java
+++ src/main/java/com/biblioteca/controllers/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.biblioteca.services.UsuarioService;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
 
