5edeff62211a1a3fca63ba41cb9e07e64cf68906
CarlosCuestaQuintas <carlos.cuesta.quintas@alumnojoyfe.iepgroup.es>
2025-06-16 12:19:11 +0200

    Desactivado el filtro JWT

--- src/main/java/com/biblioteca/security/JwtFilter.java
+++ src/main/java/com/biblioteca/security/JwtFilter.java
@@ -11,7 +11,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Component
+//@Component
 public class JwtFilter extends OncePerRequestFilter {
 	
 	@Autowired
